89ec270b17516db085602f8233e9188546ef9bad
vimalpksh <itisvimal@gmail.com>
2023-07-17 23:22:01 +0530

    JS work on progress
    Trying out different types of logics and researching on it.

--- P13 - Auto Text Generator/script.js
+++ P13 - Auto Text Generator/script.js
@@ -1,4 +1,4 @@
-const typedText = document.querySelector(".typed-text");
+const typedTextSpan = document.querySelector(".typed-text");
 const cursor = document.querySelector(".cursor");
 
 const words = ["Awesome", "Fun", "Cool", "Life", "Famous", "Wierd"];
@@ -10,8 +10,32 @@ const newLetterDelay = 2000;
 let index = 0;
 let charIndex = 0;
 
+document.addEventListener("DOMContentLoaded", () => {
+  if (words.length) {
+    setTimeout(typed, newLetterDelay);
+  }
+});
+
 function type() {
   if (charIndex < words[index].length) {
     typedTextSpan.textContent += words[index].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } else {
+    setTimeout(erase, newLetterDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typedTextSpan.textContent = words[index].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } else {
+    index++;
+    if (index >= words.lengy) {
+      index = 0;
+    }
+    setTimeout(type, typingDelay + 1100);
   }
 }
